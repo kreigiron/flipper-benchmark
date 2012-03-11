db563dae43a3b13b7fb7f63712498755551f4bf7
kreig.iron@gmail.com <kreig.iron@gmail.com@dea329fb-a017-9d31-fb60-d1d0b2b92e60>
2012-03-11 17:39:46 +0000

    structural changes

--- trunk/source/memory-benchmark.cpp
+++ trunk/source/memory-benchmark.cpp
@@ -1,15 +1,16 @@
 #include "memory-benchmark.h"
 #include "string.h"
-
+#include <iostream>
 using namespace std;
 
 memoryBenchmark::memoryBenchmark(){
+  cout << "Memory Benchmark";
 };
 
 int memoryBenchmark::doBenchmark(){
   return 0;
 };
 
-string *log(){
+string * memoryBenchmark::log(){
   return new string("");
 };
