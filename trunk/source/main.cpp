e3658cb02268e62dc138af028b7ea5698fc0bcb7
kreig.iron@gmail.com <kreig.iron@gmail.com@dea329fb-a017-9d31-fb60-d1d0b2b92e60>
2012-03-21 05:00:07 +0000

    added benchmark, not tested yet

--- trunk/source/main.cpp
+++ trunk/source/main.cpp
@@ -4,71 +4,137 @@
 #include <malloc.h>
 #include <ogcsys.h>
 #include <gccore.h>
-
+#include <dirent.h>
+#include <fat.h>
 #include <ogc/ipc.h>
+//#include <system.h>
+using namespace std;
+
      
-    static void *xfb = NULL;
-    static GXRModeObj *rmode = NULL;
-     
-    typedef void (*Loader_Entry)(void);
-    Loader_Entry loader = (Loader_Entry)0x80001800;
-     
-    void Reboot()
-    {
-            int fd = IOS_Open("/dev/stm/immediate", 0);
-            IOS_Ioctl(fd, 0x2001, NULL, 0, NULL, 0);
-            IOS_Close(fd);
+static void *xfb = NULL;
+static GXRModeObj *rmode = NULL;
+
+typedef void (*Loader_Entry)(void);
+Loader_Entry loader = (Loader_Entry)0x80001800;
+
+void Reboot()
+{
+  int fd = IOS_Open("/dev/stm/immediate", 0);
+  IOS_Ioctl(fd, 0x2001, NULL, 0, NULL, 0);
+  IOS_Close(fd);
+}
+
+bool can_open_root_fs()
+{
+  DIR *root = opendir("sd:/");
+  if(root){
+    struct dirent *temp = readdir(root);
+    if(!temp){    
+      closedir(root);
+      return true;
     }
-     
-    int main(int argc, char **argv) {
-     
-            VIDEO_Init();
-            PAD_Init();
-           
-            switch(VIDEO_GetCurrentTvMode()) {
-                    case VI_NTSC:
-                            rmode = &TVNtsc480IntDf;
-                            break;
-                    case VI_PAL:
-                            rmode = &TVPal528IntDf;
-                            break;
-                    case VI_MPAL:
-                            rmode = &TVMpal480IntDf;
-                            break;
-                    default:
-                            rmode = &TVNtsc480IntDf;
-                            break;
+  }
+  return false;
+}
+
+void die(char* message){
+  perror(message);
+  //sleep(5);
+  fatUnmount(0);
+  exit(1);
+}    
+int main(int argc, char **argv) {
+  
+  VIDEO_Init();
+  PAD_Init();
+  
+  switch(VIDEO_GetCurrentTvMode()) {
+  case VI_NTSC:
+    rmode = &TVNtsc480IntDf;
+    break;
+  case VI_PAL:
+    rmode = &TVPal528IntDf;
+    break;
+  case VI_MPAL:
+    rmode = &TVMpal480IntDf;
+    break;
+  default:
+    rmode = &TVNtsc480IntDf;
+    break;
             }
-     
-            xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
-            console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
-           
-            VIDEO_Configure(rmode);
-            VIDEO_SetNextFramebuffer(xfb);
-            VIDEO_SetBlack(FALSE);
-            VIDEO_Flush();
-            VIDEO_WaitVSync();
-            if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
-     
-            printf("\n");
-            printf("Hello World!\n");
-            printf("Return to Loader by Pressing A\n");
-            printf("Reboot by Pressing START\n");
-     
-            while(1) {
-                    VIDEO_WaitVSync();
-                    PAD_ScanPads();
-     
-                    int buttonsDown = PAD_ButtonsDown(0);
-                    if( buttonsDown & PAD_BUTTON_A ) {
-                            printf("Button A pressed.\n");
-                            loader();
-                    }
-                    if ( buttonsDown & PAD_BUTTON_START ) {
-                            printf("Start Button Pressed \n");
-                            Reboot();
-                    }
-            }
-     
-            return 0;
+  
+  xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+  console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+  
+  VIDEO_Configure(rmode);
+  VIDEO_SetNextFramebuffer(xfb);
+  VIDEO_SetBlack(FALSE);
+  VIDEO_Flush();
+  VIDEO_WaitVSync();
+  if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
+  
+  printf("\n");
+  printf("Evaluacion de rendimiento!\n");
+  printf("Regresa al loader presionando A\n");
+  printf("Reinicia presionando START\n");
+  
+  //inicializamos filesystem
+  if(!fatInitDefault()){
+    die("Imposible inicializar sistema de archivos, saliendo...\n");	      
+	    }
+  
+  if(!can_open_root_fs()){
+    die("Imposible abrir directorio raiz");
+  }
+  
+  //if(changedir("SD:/")){
+  //  die("No se puede cambiar de directorio raiz");
+  //}
+  
+  
+  while(1) {
+    VIDEO_WaitVSync();
+    PAD_ScanPads();
+		    
+    int buttonsDown = PAD_ButtonsDown(0);
+    
+    if( buttonsDown & PAD_BUTTON_UP ){
+      // ejecutamos el benchmark 1
+      printf("Prueba 1.\n");		      
     }
+    
+    if( buttonsDown & PAD_BUTTON_DOWN ){
+      // ejecutamos el benchmark 2
+      printf("Prueba 2.\n");		      
+    }
+    
+    if( buttonsDown & PAD_BUTTON_LEFT ){
+      // ejecutamos el benchmark 3
+      printf("Prueba 3.\n");		      
+    }
+    
+    if( buttonsDown & PAD_BUTTON_RIGHT ){
+      // ejecutamos el benchmark 4
+      printf("Prueba 4.\n");		      
+    }
+    
+    
+    if( buttonsDown & PAD_BUTTON_B ){
+      // escribimos logs correspondientes
+      printf("write to log .\n");		      
+    }
+    
+    
+    
+    if( buttonsDown & PAD_BUTTON_A ) {
+      printf("Button A pressed.\n");
+      loader();
+                    }
+    if ( buttonsDown & PAD_BUTTON_START ) {
+      printf("Start Button Pressed \n");
+      Reboot();
+    }
+  }
+  
+  return 0;
+}
--- trunk/source/memory-benchmark.cpp
+++ trunk/source/memory-benchmark.cpp
@@ -8,7 +8,29 @@ memoryBenchmark::memoryBenchmark(){
 };
 
 int memoryBenchmark::doBenchmark(){
-  return 0;
+  // write 1024 1byte, byte by byte, of random data chuncks from cpu to main memory}
+  
+  writeToLog("Start 1024x1byte test");
+  for(int i = 0; i < 1024; i++){
+    char c;
+    testKb[i] = c;
+  }
+  writeToLog("End 1024x1 byte test");
+ 
+  // read 1024 1kb chunks from main memory to cpu
+
+  writeToLog("Start 1024x1kb test");
+  for(int i = 0; i < 1024; i++){
+    char c;
+    testKb[i] = c;
+  }
+  writeToLog("End 1024x1kb test");
+
+  // write 1024 1kb chuncks from cpu to main memory
+  
+  // read 1024 1kb chunks from main memory to cpu
+
+ return 0;
 };
 
 string * memoryBenchmark::log(){
